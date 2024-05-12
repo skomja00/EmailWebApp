4c9ba5bc88882480214352d8944d2a605150e71e
James S <james.skomsky@gmail.com>
2024-05-12 16:18:52 -0400

    push CoreWebAPI folder

--- CoreWebAPI/obj/Debug/net8.0/EmailCoreWebAPI.AssemblyInfo.cs
+++ CoreWebAPI/obj/Debug/net8.0/EmailCoreWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailCoreWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28d4c007d36ff7d6284402641de2134d8b030d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1001a1377e5887f32d0a5ff5d0097d5428a25b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmailCoreWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmailCoreWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
